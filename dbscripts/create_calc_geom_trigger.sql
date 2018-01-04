9760d8d53a216b86af85fca9d35a0877f7c1ef7c
David <david@opengis.ch>
2018-01-04 07:28:30 +0100

    geometrychange - recalculation of refpoint/offset on update
    when the geometry of a pavement changed, we get the closest lower reference point of start and end point and the offsets
    then we recalculate the geometry out of these values
    
    if geometry not changed but start or end point or offsets, then it's like before
    
    if geometry changed and start or end point or offsets, we through an error

--- dbscripts/create_calc_geom_trigger.sql
+++ dbscripts/create_calc_geom_trigger.sql
@@ -1,19 +1,19 @@
 ﻿CREATE OR REPLACE FUNCTION chaussee_dev.calc_geom() RETURNS trigger AS $BODY$
 BEGIN
   NEW.pvl_line_geom = ST_LineSubstring(
-	(
-	SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid 
-	),
-	(
-	ST_LineLocatePoint( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ), (SELECT sec_refpoint_geom FROM chaussee_dev.t_sectors WHERE sec_iliid = NEW.pvl_start_sec_iliid ) )
-	+
-	NEW.pvl_start_u/ST_Length( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ) )
-	),
-	(
-	ST_LineLocatePoint( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ), (SELECT sec_refpoint_geom FROM chaussee_dev.t_sectors WHERE sec_iliid = NEW.pvl_end_sec_iliid ) )
-	+
-	NEW.pvl_end_u/ST_Length( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ) )
-	)
+    (
+    SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid 
+    ),
+    (
+    ST_LineLocatePoint( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ), (SELECT sec_refpoint_geom FROM chaussee_dev.t_sectors WHERE sec_iliid = NEW.pvl_start_sec_iliid ) )
+    +
+    NEW.pvl_start_u/ST_Length( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ) )
+    ),
+    (
+    ST_LineLocatePoint( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ), (SELECT sec_refpoint_geom FROM chaussee_dev.t_sectors WHERE sec_iliid = NEW.pvl_end_sec_iliid ) )
+    +
+    NEW.pvl_end_u/ST_Length( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ) )
+    )
   );
   RETURN NEW;
 END; $BODY$
@@ -27,11 +27,78 @@ CREATE TRIGGER calc_geom_before_insert
   FOR EACH ROW
   EXECUTE PROCEDURE chaussee_dev.calc_geom();
 
+CREATE OR REPLACE FUNCTION chaussee_dev.calc_geom_on_update() RETURNS trigger AS $BODY$
+BEGIN
+
+  IF NOT( NEW.pvl_line_geom = OLD.pvl_line_geom ) THEN
+  
+    IF NOT( NEW.pvl_start_sec_iliid = OLD.pvl_start_sec_iliid AND NEW.pvl_end_sec_iliid  = OLD.pvl_end_sec_iliid AND NEW.pvl_start_u = OLD.pvl_start_u AND NEW.pvl_end_u = OLD.pvl_end_u ) THEN
+      RAISE EXCEPTION 'Change of geometry and refpoints/offsets at same transaction not allowed';
+    END IF;
+        
+    NEW.pvl_start_sec_iliid =
+    (
+      SELECT sec_iliid 
+      FROM chaussee_dev.t_sectors 
+      WHERE sec_asg_iliid = NEW.pvl_asg_iliid 
+      AND ST_LineLocatePoint( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ), sec_refpoint_geom ) <= ST_LineLocatePoint( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ), ST_StartPoint( NEW.pvl_line_geom ) )
+      ORDER BY ST_LineLocatePoint( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ), sec_refpoint_geom ) DESC
+      LIMIT 1
+    );
+    
+    NEW.pvl_start_u = 
+    ST_Length( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ) )
+    *
+    (
+     ( ST_LineLocatePoint( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ), ST_StartPoint( NEW.pvl_line_geom ) ) )
+      -
+     ( ST_LineLocatePoint( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ), (SELECT sec_refpoint_geom FROM chaussee_dev.t_sectors WHERE sec_iliid = NEW.pvl_start_sec_iliid ) ) )
+    );
+
+    NEW.pvl_end_sec_iliid = 
+    (
+      SELECT sec_iliid 
+      FROM chaussee_dev.t_sectors 
+      WHERE sec_asg_iliid = NEW.pvl_asg_iliid 
+      AND ST_LineLocatePoint( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ), sec_refpoint_geom ) <= ST_LineLocatePoint( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ), ST_EndPoint( NEW.pvl_line_geom ) )
+      ORDER BY ST_LineLocatePoint( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ), sec_refpoint_geom ) DESC
+      LIMIT 1
+    );
+    
+    NEW.pvl_end_u = 
+    ST_Length( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ) )
+    *
+    (
+      ( ST_LineLocatePoint( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ), ST_EndPoint( NEW.pvl_line_geom ) ) )
+      -
+      ( ST_LineLocatePoint( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ), (SELECT sec_refpoint_geom FROM chaussee_dev.t_sectors WHERE sec_iliid = NEW.pvl_end_sec_iliid ) ) )
+    );
+  END IF;
+  
+  NEW.pvl_line_geom = ST_LineSubstring(
+    (
+      SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid 
+    ),
+    (
+      ST_LineLocatePoint( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ), (SELECT sec_refpoint_geom FROM chaussee_dev.t_sectors WHERE sec_iliid = NEW.pvl_start_sec_iliid ) )
+      +
+      NEW.pvl_start_u/ST_Length( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ) )
+    ),
+    (
+      ST_LineLocatePoint( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ), (SELECT sec_refpoint_geom FROM chaussee_dev.t_sectors WHERE sec_iliid = NEW.pvl_end_sec_iliid ) )
+      +
+      NEW.pvl_end_u/ST_Length( (SELECT asg_geom FROM chaussee_dev.t_axissegments WHERE asg_iliid = NEW.pvl_asg_iliid ) )
+    )
+  );
+  
+  RETURN NEW;
+END; $BODY$
+LANGUAGE plpgsql;
+
 DROP TRIGGER IF EXISTS calc_geom_before_update ON chaussee_dev.t_pavement_layers;
 
 CREATE TRIGGER calc_geom_before_update
   BEFORE UPDATE
   ON chaussee_dev.t_pavement_layers
   FOR EACH ROW
-  EXECUTE PROCEDURE chaussee_dev.calc_geom();
-  
+  EXECUTE PROCEDURE chaussee_dev.calc_geom_on_update();
